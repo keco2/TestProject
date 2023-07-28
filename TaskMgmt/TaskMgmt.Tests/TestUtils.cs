819f0b463536d6bdc0ed762a860a45ac5c59c51d
keco2 <75701934+keco2@users.noreply.github.com>
2023-07-28 15:40:02 +0200

    Add NUnit project for tests

---
+++ TaskMgmt/TaskMgmt.Tests/TestUtils.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMgmt.Tests
+{
+    public class TestUtils
+    {
+    }
+}
