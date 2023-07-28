3c9755b6b5c299eaeaf892858320554ab5e3964a
keco2 <75701934+keco2@users.noreply.github.com>
2023-07-28 15:43:46 +0200

    Add Unit-variations unit tests

---
+++ TaskMgmt/TaskMgmt.Tests/UnitTests/BLL/UsageUnitTest.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMgmt.Model;
+
+namespace TaskMgmt.Tests.UnitTests.BLL
+{
+    [TestFixture]
+    public class UsageUnitTest
+    {
+        [Test]
+        public void AllUnitVariations_ReturnsAll()
+        {
+            // Setup
+            var unitSetup = new UnitSetup();
+
+            //Act
+            var result = unitSetup.GetVariations().OrderBy(u => u);
+            var expected = new string[] { "g", "mg", "kg", "l", "ml", "m", "mm", "km" }.OrderBy(u => u);
+
+            //Assert
+            Assert.AreEqual(1, 1);
+        }
+
+        [Test]
+        public void UnitGetVariationsOfKg_Returns_g_mg_kg()
+        {
+            // Setup
+            var unitSetup = new UnitSetup();
+
+            //Act
+            var result = unitSetup.GetVariations("g");
+            var expected = new string[] { "g", "mg", "kg" };
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void UnitGetVariationsOfLiter_Returns_ml_l()
+        {
+            // Setup
+            var unitSetup = new UnitSetup();
+
+            //Act
+            var result = unitSetup.GetVariations("l");
+            var expected = new string[] { "ml", "l" };
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void UnitBase_ReturnsOnlyBaseUnits()
+        {
+            // Setup
+            var unitSetup = new UnitSetup();
+
+            //Act
+            var result = unitSetup.GetBaseUnits().OrderBy(u => u);
+            var expected = new string[] { "g", "m", "l" }.OrderBy(u => u);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
