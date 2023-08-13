625efaff5330327c70d0da18f9a2420185983fa9
keco2 <75701934+keco2@users.noreply.github.com>
2023-08-13 18:50:31 +0200

    Add TaskRepository UnitTests

---
+++ TaskMgmt/TaskMgmt.Tests/DAL/TaskRepositoryTests.cs
@@ -0,0 +1,173 @@
+﻿using NUnit.Framework;
+using TaskMgmt.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMgmt.DAL.Interface;
+using NSubstitute;
+using NLog;
+using System.Data.Entity;
+using TaskMgmt.Tests;
+using System.Data.Entity.Infrastructure;
+
+namespace TaskMgmt.DAL.UnitTests
+{
+    [TestFixture]
+    public class TaskRepositoryTests
+    {
+        [Test]
+        public void InsertItemTest_Inserting_InsertsButNotSaves()
+        {
+            // Setup
+
+            var dbContextMock = Substitute.For<ITaskMgmtDbContext>();
+            var loggerMock = Substitute.For<ILogger>();
+            var dbsetMock = Substitute.For<DbSet<TaskEntity>>();
+            var taskEntityStub = Substitute.For<TaskEntity>();
+
+            dbContextMock.Set<TaskEntity>().Returns(dbsetMock);
+
+            var taskRepository = new TaskRepository(dbContextMock, loggerMock);
+
+            dbsetMock.ClearReceivedCalls();
+            dbContextMock.ClearReceivedCalls();
+            loggerMock.ClearReceivedCalls();
+
+            // Act
+
+            taskRepository.InsertItem(taskEntityStub);
+
+            // Assert
+
+            dbsetMock.ReceivedWithAnyArgs(1);
+            dbsetMock.Received(1).Add(taskEntityStub);
+
+            dbContextMock.ReceivedWithAnyArgs(0);
+            dbContextMock.DidNotReceive().SaveChanges();
+
+            loggerMock.ReceivedWithAnyArgs(1);
+            loggerMock.Received(1).Info(taskEntityStub.ID.ToString());
+        }
+
+        [Test]
+        public void GetItemsByIDTest_WithAnyId_ReturnsExpectedItem()
+        {
+            // Setup
+
+            int anyNumber = 3;
+            var dbQueryMockWithTestData = TestDataGenerator.GenerateDbQueryMockWithTestData<TaskEntity>(anyNumber);
+            var dbsetMock = Substitute.For<DbSet<TaskEntity>>();
+            var dbContextMock = Substitute.For<ITaskMgmtDbContext>();
+            var loggerMock = Substitute.For<ILogger>();
+
+            dbsetMock.AsNoTracking().Returns(dbQueryMockWithTestData);
+            dbContextMock.Set<TaskEntity>().Returns(dbsetMock);
+
+            dbsetMock.ClearReceivedCalls();
+            dbContextMock.ClearReceivedCalls();
+            loggerMock.ClearReceivedCalls();
+
+            var taskRepository = new TaskRepository(dbContextMock, loggerMock);
+            var anyItem = dbQueryMockWithTestData.Last();
+
+            // Act
+
+            IEnumerable<TaskEntity> result = taskRepository.GetItemsByID(anyItem.ID);
+
+            // Assert
+
+            Assert.AreEqual(1, result.Count(), "Single item is expected.");
+            Assert.AreSame(anyItem, result.Single());
+
+            dbsetMock.ReceivedWithAnyArgs(0);
+            dbContextMock.ReceivedWithAnyArgs(0);
+            dbContextMock.DidNotReceive().SaveChanges();
+
+            loggerMock.ReceivedWithAnyArgs(1);
+            loggerMock.Received(1).Info(anyItem.ID.ToString());
+        }
+
+        [Test]
+        public void GetItemsTest_ReturnsAllItems()
+        {
+            // Setup
+
+            int anyNumber = 3;
+            var dbQueryMockWithTestData = TestDataGenerator.GenerateDbQueryMockWithTestData<TaskEntity>(anyNumber);
+            var dbsetMock = Substitute.For<DbSet<TaskEntity>>();
+            var dbContextMock = Substitute.For<ITaskMgmtDbContext>();
+            var loggerMock = Substitute.For<ILogger>();
+
+            dbsetMock.AsNoTracking().Returns(dbQueryMockWithTestData);
+            dbContextMock.Set<TaskEntity>().Returns(dbsetMock);
+
+            dbsetMock.ClearReceivedCalls();
+            dbContextMock.ClearReceivedCalls();
+            loggerMock.ClearReceivedCalls();
+
+            var taskRepository = new TaskRepository(dbContextMock, loggerMock);
+
+            // Act
+
+            IEnumerable<TaskEntity> result = taskRepository.GetItems();
+
+            // Assert
+
+            Assert.AreEqual(anyNumber, result.Count());
+
+            dbsetMock.ReceivedWithAnyArgs(0);
+            dbContextMock.ReceivedWithAnyArgs(0);
+            dbContextMock.DidNotReceive().SaveChanges();
+
+            loggerMock.ReceivedWithAnyArgs(1);
+            loggerMock.Received(1).Info("All");
+        }
+
+        [Ignore("Mocking not possible for DbEntityEntry")]
+        [Test]
+        public void UpdateItem_ChangedItem_UpdatesItemWithThatId()
+        {
+            // Setup
+
+            int anyNumber = 3;
+            var dbQueryMockWithTestData = TestDataGenerator.GenerateDbQueryMockWithTestData<TaskEntity>(anyNumber);
+            var dbsetMock = Substitute.For<DbSet<TaskEntity>>();
+            var dbContextMock = Substitute.For<ITaskMgmtDbContext>();
+            var loggerMock = Substitute.For<ILogger>();
+
+            dbsetMock.AsNoTracking().Returns(dbQueryMockWithTestData);
+            dbContextMock.Set<TaskEntity>().Returns(dbsetMock);
+
+            dbsetMock.ClearReceivedCalls();
+            dbContextMock.ClearReceivedCalls();
+            loggerMock.ClearReceivedCalls();
+
+            var taskRepository = new TaskRepository(dbContextMock, loggerMock);
+
+            var anyItemRecreated = new TaskEntity();
+            anyItemRecreated.ID = dbQueryMockWithTestData.Last().ID;
+            anyItemRecreated.Name = "Changed";
+
+            // Act
+
+            taskRepository.UpdateItem(anyItemRecreated);
+            //   A) NullReferenceException     -> When setting context.Entry(task).State
+            //   B) If mocking DbEntityEntry<> -> Mock-Exception:Parent has no default constructor
+
+            // Assert
+
+            Assert.AreSame(anyItemRecreated.Name, dbsetMock.Where(e => e.ID == anyItemRecreated.ID).Single().Name);
+
+            dbsetMock.ReceivedWithAnyArgs(1);
+            dbsetMock.Attach(anyItemRecreated).Received(1);
+            dbContextMock.ReceivedWithAnyArgs(1);
+            dbContextMock.Entry(anyItemRecreated).Received(1);
+            dbContextMock.DidNotReceive().SaveChanges();
+
+            loggerMock.ReceivedWithAnyArgs(1);
+            loggerMock.Received(1).Info(anyItemRecreated.ID.ToString());
+        }
+    }
+}
---
+++ TaskMgmt/TaskMgmt.Tests/TestDataGenerator.cs
@@ -0,0 +1,52 @@
+﻿using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskMgmt.DAL;
+
+namespace TaskMgmt.Tests
+{
+    public static class TestDataGenerator
+    {
+        public static IEnumerable<T> GenerateListOfT<T>(int count)
+        {
+            var data = new List<T>();
+
+            for (int i = 0; i < count; i++)
+            {
+                data.Add(GenerateT<T>(i));
+            }
+
+            return data;
+        }
+
+        public static T GenerateT<T>(int withId)
+        {
+            var obj = Activator.CreateInstance<T>();
+            var prop = typeof(T).GetProperty("ID", typeof(Guid));
+            if (prop == null)
+            {
+                throw new NotSupportedException($"{nameof(T)} not supported. Type must implement property called ID (of type:Guid).");
+            }
+            prop.SetValue(obj, new Guid(withId.ToString().PadLeft(32, '0')));
+            return obj;
+        }
+
+        public static DbQuery<T> GenerateDbQueryMockWithTestData<T>(int count) where T : class
+        {
+            var mockSet = Substitute.For<DbSet<T>, IQueryable<T>>();
+            var dataQueryable = GenerateListOfT<T>(count).AsQueryable();
+
+            ((IQueryable<T>)mockSet).Provider.Returns(dataQueryable.Provider);
+            ((IQueryable<T>)mockSet).Expression.Returns(dataQueryable.Expression);
+            ((IQueryable<T>)mockSet).ElementType.Returns(dataQueryable.ElementType);
+            ((IQueryable<T>)mockSet).GetEnumerator().Returns(dataQueryable.GetEnumerator());
+
+            return mockSet;
+        }
+    }
+}
