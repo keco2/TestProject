f60d3ba6d2166c5786b5ca96f1c83c5e20a4146b
keco2 <75701934+keco2@users.noreply.github.com>
2023-08-13 21:17:48 +0200

    Add missing RequiredAttribute into TaskEntity

--- TaskMgmt/TaskMgmt.DAL/Entities/TaskEntity.cs
+++ TaskMgmt/TaskMgmt.DAL/Entities/TaskEntity.cs
@@ -8,6 +8,7 @@ namespace TaskMgmt.DAL
         [Required]
         public Guid ID { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
         public string Description { get; set; }
